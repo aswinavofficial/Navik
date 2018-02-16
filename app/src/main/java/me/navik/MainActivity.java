93bfdfe7eeaebed986430ce215ceec9059891020
aswinavofficial <aswinavofficial@gmail.com>
2018-02-16 23:45:06 +0530

    new floor view

--- app/src/main/java/me/navik/MainActivity.java
+++ app/src/main/java/me/navik/MainActivity.java
@@ -1,7 +1,12 @@
 package me.navik;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 .username("gola")
                 .build();
 
-        findClient.track()
+   /*     findClient.track()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<String>() {
@@ -52,7 +57,38 @@ public class MainActivity extends AppCompatActivity {
                         Timber.d("Location: " + location);
                         t.setText(location);
                     }
-                });
+                }); */
+
+
+        if (internet_connection()){
+            t.setText("Connected");
+          findClient.track()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<String>() {
+                        @Override
+                        public void accept(String location) {
+                            Timber.d("Location: " + location);
+                            t.setText(location);
+                        }
+                    });
+
+
+        }else{
+            t.setText("No Internet");
+            //create a snackbar telling the user there is no internet connection and issuing a chance to reconnect
+            final Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                    "No internet connection.",
+                    Snackbar.LENGTH_SHORT);
+            snackbar.setActionTextColor(ContextCompat.getColor(getApplicationContext(),
+                    R.color.colorPrimary));
+            snackbar.setAction(R.string.try_again, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //recheck internet connection and call DownloadJson if there is internet
+                }
+            }).show();
+        }
 
     }
 
@@ -66,5 +102,17 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent i = new Intent(getApplicationContext(),Track.class);
         startActivity(i);
+    }
+
+
+    boolean internet_connection(){
+        //Check if connected to internet, output accordingly
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
     }
 }
--- app/src/main/java/me/navik/Track.java
+++ app/src/main/java/me/navik/Track.java
@@ -1,7 +1,12 @@
 package me.navik;
 
 import android.Manifest;
+import android.content.Context;
 import android.graphics.PointF;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -41,65 +46,104 @@ public class Track extends AppCompatActivity {
                 .group("ideathon")
                 .username("gola")
                 .build();
-
-        findClient.track()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<String>() {
-                    @Override
-                    public void accept(String location) {
-                        Timber.d("Location: " + location);
-                        String [] cor = location.split("_", 2);
-                        Float[] par=new Float[cor.length];
-                        int i=0;
-                        for(String str:cor){
-                            par[i]=Float.parseFloat(str);//Exception in this line
-                            i++;
+        if (internet_connection()){
+            findClient.track()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<String>() {
+                        @Override
+                        public void accept(String location) {
+                            Timber.d("Location: " + location);
+                            String [] cor = location.split("_", 2);
+                            Float[] par=new Float[cor.length];
+                            int i=0;
+                            for(String str:cor){
+                                par[i]=Float.parseFloat(str);//Exception in this line
+                                i++;
+                            }
+                            // imageView.setPin(new PointF(500f, 500f));
+                            imageView.setPin(new PointF(par[0], par[1]));
                         }
-                        // imageView.setPin(new PointF(500f, 500f));
-                        imageView.setPin(new PointF(par[0], par[1]));
-
-                    }
+                    });
 
 
+        }else{
+            //create a snackbar telling the user there is no internet connection and issuing a chance to reconnect
+            final Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                    "No internet connection.",
+                    Snackbar.LENGTH_SHORT);
+            snackbar.setActionTextColor(ContextCompat.getColor(getApplicationContext(),
+                    R.color.colorPrimary));
+            snackbar.setAction(R.string.try_again, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //recheck internet connection and call DownloadJson if there is internet
+                }
+            }).show();
+        }
 
-                });
 
     }
 
 
     public void livelocation(View view)
     {
-
-        findClient.track()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<String>() {
-                    @Override
-                    public void accept(String location) {
-                        Timber.d("Location: " + location);
-                        String [] cor = location.split("_", 2);
-                        Float[] par=new Float[cor.length];
-                        int i=0;
-                        for(String str:cor){
-                            par[i]=Float.parseFloat(str);//Exception in this line
-                            i++;
+        if (internet_connection()){
+            findClient.track()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<String>() {
+                        @Override
+                        public void accept(String location) {
+                            Timber.d("Location: " + location);
+                            String [] cor = location.split("_", 2);
+                            Float[] par=new Float[cor.length];
+                            int i=0;
+                            for(String str:cor){
+                                par[i]=Float.parseFloat(str);//Exception in this line
+                                i++;
+                            }
+                            // imageView.setPin(new PointF(500f, 500f));
+                            imageView.setPin(new PointF(par[0], par[1]));
                         }
-                        // imageView.setPin(new PointF(500f, 500f));
-                        imageView.setPin(new PointF(par[0], par[1]));
-
-                    }
+                    });
 
 
+        }else{
+            //create a snackbar telling the user there is no internet connection and issuing a chance to reconnect
+            final Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                    "No internet connection.",
+                    Snackbar.LENGTH_SHORT);
+            snackbar.setActionTextColor(ContextCompat.getColor(getApplicationContext(),
+                    R.color.colorPrimary));
+            snackbar.setAction(R.string.try_again, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //recheck internet connection and call DownloadJson if there is internet
+                }
+            }).show();
+        }
 
-                });
 
     }
 
+
     public void dest(View view)
     {
-        imageView.setPin(new PointF(960,525));
 
+        imageView.setPin(new PointF(1950, 259));
+
+    }
+
+    boolean internet_connection(){
+        //Check if connected to internet, output accordingly
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
     }
 
 
